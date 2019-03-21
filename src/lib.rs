d6dbc4ac6528ee565daaba3071f6e46a260e2c11
vthriller <unixway.drive+git@gmail.com>
2019-03-21 15:49:36 +0300

    I already anticipate this question…

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,12 @@
 #![feature(hash_raw_entry)]
 #![feature(test)]
 
+/*
+Yes, the following mods are copy-pasted.
+There is no common crate that we can depend on. We can write one for ourselves with thin-wrapping `impl`, but is it worth it?
+And don't even tell me about macros. I've been here, it falls apart when there's need to `impl` for both `Foo<A, B>` and `Bar<A, B, C>` (note the extra type argument).
+*/
+
 pub mod std_hash;
 #[cfg(feature = "hashbrown")]
 pub mod hashbrown;
